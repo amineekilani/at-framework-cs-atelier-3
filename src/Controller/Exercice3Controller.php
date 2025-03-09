5183c867e17ce3aae9a22335ca952039171a0c84
Amine Kilani <aminekilani901@gmail.com>
2025-03-09 14:30:10 +0100

    Version finale

--- src/Controller/Exercice3Controller.php
+++ src/Controller/Exercice3Controller.php
@@ -6,39 +6,35 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class GammeController extends AbstractController
+final class Exercice3Controller extends AbstractController
 {
-    #[Route('/gamme', name: 'app_gamme')]
+    #[Route('/exercice3', name: 'app_exercice3')]
     public function index(): Response
     {
-        return $this->render('gamme/Lister.html.twig', [
-            'controller_name' => 'GammeController',
+        return $this->render('exercice3/index.html.twig', [
+            'controller_name' => 'Exercice3Controller',
         ]);
     }
-    #[Route('/gamme/lister', name: 'app_gamme_lister')]
+    #[Route('/exercice3/lister', name: 'app_exercice3_lister')]
     public function lister(): Response
     {
-        $Gamme=["ordinateur","vidéoprojecteur","imprimante","accessoire"];
-        return $this->render('gamme/Lister.html.twig', ['gamme'=>$Gamme]);
+        $Articles=[ 
+            ['titre' => 'Le premier article', 'resume' => 'Résumé du premier article', 'date_publication' => '2024-01-01', 'auteur' => 'Meriam'],  
+            ['titre' => 'Le deuxième article ', 'resume' => 'Résumé du deuxième article', 'date_publication' => '2022-05-15', 'auteur' => 'Ahmed'],  
+            ['titre' => 'Le troisième article ', 'resume' => 'Résumé du troisième resume', 'date_publication' => '2020-03-10', 'auteur' => 'Nidhal'], 
+        ];
+        return $this->render('exercice3/index.html.twig', ['article'=>$Articles]);
     }
-    #[Route('/gamme/lister2', name: 'app_gamme_lister2')]
-    public function lister2(): Response
+    #[Route('/exercice3/lister/{auteur}', name: 'app_article_lister_auteur')]
+    public function listeParAuteur(String $auteur): Response
     {
         $Articles=[ 
             ['titre' => 'Le premier article', 'resume' => 'Résumé du premier article', 'date_publication' => '2024-01-01', 'auteur' => 'Meriam'],  
             ['titre' => 'Le deuxième article ', 'resume' => 'Résumé du deuxième article', 'date_publication' => '2022-05-15', 'auteur' => 'Ahmed'],  
             ['titre' => 'Le troisième article ', 'resume' => 'Résumé du troisième resume', 'date_publication' => '2020-03-10', 'auteur' => 'Nidhal'], 
         ];
-        return $this->render('gamme/Lister2.html.twig', ['article'=>$Articles]);
-    }
-    #[Route('/gamme/lister2/{auteur}', name: 'app_gamme_lister2_auteur')]
-    public function lister2auteur(String $auteur): Response
-    {
-        $Articles=[ 
-            ['titre' => 'Le premier article', 'resume' => 'Résumé du premier article', 'date_publication' => '2024-01-01', 'auteur' => 'Meriam'],  
-            ['titre' => 'Le deuxième article ', 'resume' => 'Résumé du deuxième article', 'date_publication' => '2022-05-15', 'auteur' => 'Ahmed'],  
-            ['titre' => 'Le troisième article ', 'resume' => 'Résumé du troisième resume', 'date_publication' => '2020-03-10', 'auteur' => 'Nidhal'], 
-        ];
-        return $this->render('gamme/Lister2.html.twig', ['article'=>array_filter($Articles, fn($article) => $article['auteur'] === $auteur)]);
+
+        return $this->render('exercice3/index.html.twig',
+        ['article'=>array_filter($Articles, fn($article) => $article['auteur'] === $auteur)]);
     }
 }
